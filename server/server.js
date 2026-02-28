fb35beb51cb8eeca63f306287cfb78f78a2840de
rukmani104 <rukmani.sharma3610@gmail.com>
2026-02-28 13:00:50 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -8,12 +8,6 @@ const path = require("path");
 const app = express();
 app.use(cors());
 
-// Serve React build folder
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
 const server = http.createServer(app);
 
@@ -190,5 +184,5 @@ io.on("connection", (socket) => {
 // ===============================
 // START SERVER
 // ===============================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
